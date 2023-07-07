63a27043fd74c5fca2bcc92037f3efea8120c765
Volodya Kushch <110915611+Volodya460@users.noreply.github.com>
2023-07-07 20:10:08 +0300

    auth

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config();
+const cors = require("cors");
+require("dotenv").config();
 
-const authRouter = require('./route')
+const authRouter = require("./route/auth");
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
-
-
-// тут повинні бути роути
+app.use(express.static("public"));
 
 // тут повинні бути роути
-
+app.use("/users", authRouter);
+// тут повинні бути роути
 
 app.use((req, res) => {
-    res.status(404).json({ message: 'not found' });
+  res.status(404).json({ message: "not found" });
 });
 app.use((err, req, res, next) => {
-    const { status = 500, message = 'Server error' } = err;
-    res.status(status).json({ message: message });
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message: message });
 });
 
 module.exports = app;
---
+++ middlewares/validateBody.js
@@ -0,0 +1,44 @@
+const { HttpError } = require("../helpers");
+
+const validateBodyPost = (schema) => {
+  const func = (req, res, next) => {
+    const { error } = schema.validate(req.body);
+    const bodyLength = Object.keys(req.body).length;
+
+    if (bodyLength === 0) {
+      throw HttpError(400, "missing fields");
+    }
+    if (error) {
+      const errorMesagge =
+        error.message
+          .split("is required")
+          .join("")
+          .split('"')
+          .join("")
+          .split(",")
+          .toString() || "";
+
+      next(HttpError(400, ` missing required ${errorMesagge} field`));
+    }
+    next();
+  };
+  return func;
+};
+
+const validateUsersPatch = (schema) => {
+  const func = (req, res, next) => {
+    const { error } = schema.validate(req.body);
+
+    if (error) {
+      next(HttpError(400, "missing field subscription"));
+    }
+    next();
+  };
+  return func;
+};
+
+module.exports = {
+  validateBodyPost,
+
+  validateUsersPatch,
+};
