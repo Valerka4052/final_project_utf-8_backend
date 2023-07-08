9e9fc045028967b4006d737b8f9c5379c9824164
Valerka4052 <114054150+Valerka4052@users.noreply.github.com>
2023-07-08 13:52:39 +0300

    add operations

--- route/operations.js
+++ route/operations.js
@@ -1,6 +1,25 @@
 const express = require("express");
 const operationsRouter = express.Router();
-const { getListsByCategories, getAllCategories, getRecipeById, getListsByCategoriesPage,searchRecipes,getAllIngredients,getRecipesByIngredient } = require('../controllers/operations');
+const {
+    getListsByCategories,
+    getAllCategories,
+    getRecipeById,
+    getListsByCategoriesPage,
+    searchRecipes,
+    getAllIngredients,
+    getRecipesByIngredient,
+    getRecipeByUser,
+    addRecipe,
+    deleteRecipe,
+    getFavoriteRecipeByUser,
+    addRecipeToFavorite,
+    deleteRecipeFromFavorite,
+    getPouplarRecipes,
+    getShoppingList,
+    addProductToSoppingList,
+    removeProductFromSoppingList,
+} = require('../controllers/operations');
+
 operationsRouter.get('/recipes/main-page', getListsByCategories);
 operationsRouter.get('/recipes/category-list', getAllCategories);
 operationsRouter.get('/recipes/{category}', getListsByCategoriesPage);
@@ -8,7 +27,16 @@ operationsRouter.get('/recipes/:id', getRecipeById);
 operationsRouter.post('/search', searchRecipes);
 operationsRouter.get('/ingredients/list', getAllIngredients);
 operationsRouter.post('/ingredients', getRecipesByIngredient);
-
+operationsRouter.get('/ownRecipes', getRecipeByUser);
+operationsRouter.post('/ownRecipes', addRecipe);
+operationsRouter.delete('/ownRecipes', deleteRecipe);
+operationsRouter.get('/favorite', getFavoriteRecipeByUser);
+operationsRouter.post('/favorite', addRecipeToFavorite);
+operationsRouter.patch('/favorite', deleteRecipeFromFavorite);
+operationsRouter.get('/popular-recipe', getPouplarRecipes);
+operationsRouter.get('/shopping-list', getShoppingList);
+operationsRouter.post('/shopping-list', addProductToSoppingList);
+operationsRouter.patch('/shopping-list', removeProductFromSoppingList);
 
 
 
