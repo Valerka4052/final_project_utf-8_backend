ea6e3e753a4965575349449a6d80adeb71f683e1
Volodya Kushch <110915611+Volodya460@users.noreply.github.com>
2023-07-11 15:54:36 +0300

    auth/Gravatar

--- models/user.js
+++ models/user.js
@@ -31,7 +31,7 @@ const userSchema = new Schema(
 
       default: [],
     },
-    avatarURL: { type: String },
+    avatarURL: { type: String, default: null },
     // verify: {
     //   type: Boolean,
     //   default: false,
