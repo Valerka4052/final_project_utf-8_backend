ecd64fe68ab43450b26ff1a3f624a62f7ae8b3e4
Valerka4052 <114054150+Valerka4052@users.noreply.github.com>
2023-07-13 21:38:02 +0300

    back 23h

--- controllers/auth.js
+++ controllers/auth.js
@@ -110,7 +110,7 @@ const login = async (req, res) => {
   const payload = {
     id: user._id,
   };
-  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1m" });
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
   await User.findByIdAndUpdate(user._id, { token });
   res.status(200).json({
     token,
