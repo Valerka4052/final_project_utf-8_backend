53e8e44d77bc97ed875db6663ade61de43583497
Valerka4052 <114054150+Valerka4052@users.noreply.github.com>
2023-07-13 21:24:57 +0300

    add token

--- controllers/auth.js
+++ controllers/auth.js
@@ -110,7 +110,7 @@ const login = async (req, res) => {
   const payload = {
     id: user._id,
   };
-  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1m" });
   await User.findByIdAndUpdate(user._id, { token });
   res.status(200).json({
     token,
